d46cf73d3671001962b86099b7025d8f1712fbe1
Julian Castro <juliancastro18k@gmail.com>
2020-11-17 15:55:41 -0300

    Landscape Mobile

--- js/landscape.js
+++ js/landscape.js
@@ -1,19 +1,19 @@
+const frame = document.querySelector('#frame');
 const elem = document.querySelector('#content');
 const body = document.querySelector('body');
 const btnImg = document.querySelector('#btn-img');
 
 document.onreadystatechange = hideLoading;
-body.addEventListener("mousemove", parallax);
+body.addEventListener("mousemove", getClickCoord);
 btnImg.addEventListener("click", imgResize);
 
-function parallax(e) {
+function parallax(xCoord) {
     let w = window.innerWidth / 2;
-    let mouseX = e.clientX;
     // le agregué +10 para que se vea mas interesante al redimensionar
-    let depth1 = `${50-(mouseX-w)*0.005+10}% ${50}%`;
-    let depth2 = `${50-(mouseX-w)*0.01+10}% ${50}%`;
-    let depth3 = `${50-(mouseX-w)*0.02+10}% ${50}%`;
-    let depth4 = `${50-(mouseX-w)*0.04+10}% ${50}%`;
+    let depth1 = `${50-(xCoord-w)*0.005+10}% ${50}%`;
+    let depth2 = `${50-(xCoord-w)*0.01+10}% ${50}%`;
+    let depth3 = `${50-(xCoord-w)*0.02+10}% ${50}%`;
+    let depth4 = `${50-(xCoord-w)*0.04+10}% ${50}%`;
     let x = `${depth4},${depth3},${depth2},${depth1}`;
     // console.log(x);
     elem.style.backgroundPosition = x;
@@ -21,6 +21,7 @@ function parallax(e) {
 
 function imgResize(e) {
     elem.classList.toggle('fit');
+    frame.classList.toggle('fit');
 
     if(elem.classList.contains('fit')){
         btnImg.textContent = "Crop to fit";
@@ -34,4 +35,20 @@ function hideLoading() {
     if (state == 'complete') {
             document.querySelector('.loading').style.display="none";
     }
+}
+
+body.addEventListener("touchstart", getTouchCoord);
+body.addEventListener("touchend", getTouchCoord);
+body.addEventListener("touchmove", getTouchCoord);
+
+function getTouchCoord(e) {
+    const evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+    const touch = evt.touches[0] || evt.changedTouches[0];
+    const xCoord = touch.pageX;
+    parallax(xCoord);
+};
+
+function getClickCoord(e) {
+    const xCoord = e.clientX;
+    parallax(xCoord);
 }
